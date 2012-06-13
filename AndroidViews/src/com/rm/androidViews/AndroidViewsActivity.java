cb3c3354d5b99219a7a5f8c25eda3a9221496c91
shuntaro@DeskTop-Work <shuntaro@H0947.rgk.ricoh.co.jp>
2012-06-13 16:59:09 +0900

    first commit

---
+++ AndroidViews/src/com/rm/androidViews/AndroidViewsActivity.java
@@ -0,0 +1,13 @@
+package com.rm.androidViews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidViewsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
