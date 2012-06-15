d755d274915c79b12955b6eca9b9b0f0b434d6f5
RMagic <sim2bird@gmail.com>
2012-06-15 17:48:51 +0900

    now coding ... P.156

--- AndroidViews/src/com/rm/androidViews/AndroidViewsActivity.java
+++ AndroidViews/src/com/rm/androidViews/AndroidViewsActivity.java
@@ -2,6 +2,9 @@ package com.rm.androidViews;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AndroidViewsActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,17 @@ public class AndroidViewsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	menu.add(0, 0, Menu.NONE, "AutoComplete");
+    	menu.add(0, 1, Menu.NONE, "Button");
+    	menu.add(0, 2, Menu.NONE, "CheckBox");
+    	menu.add(0, 3, Menu.NONE, "EditText");
+    	menu.add(0, 4, Menu.NONE, "RadioGroup");
+    	menu.add(0, 5, Menu.NONE, "Spinner");
+    	return true;
     }
 }
