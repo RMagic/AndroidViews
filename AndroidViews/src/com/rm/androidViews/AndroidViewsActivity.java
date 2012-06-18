9136aa6486021de4cf22039a80c333485fcef8af
shuntaro@DeskTop-Work <shuntaro@H0947.rgk.ricoh.co.jp>
2012-06-18 17:34:50 +0900

    coding...,P165

--- AndroidViews/src/com/rm/androidViews/AndroidViewsActivity.java
+++ AndroidViews/src/com/rm/androidViews/AndroidViewsActivity.java
@@ -1,5 +1,6 @@
 package com.rm.androidViews;
 
+import android.R.bool;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
@@ -14,7 +15,7 @@ public class AndroidViewsActivity extends Activity {
         setContentView(R.layout.main);
     }
     
-    @override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
     	super.onCreateOptionsMenu(menu);
     	menu.add(0, 0, Menu.NONE, "AutoComplete");
@@ -24,5 +25,32 @@ public class AndroidViewsActivity extends Activity {
     	menu.add(0, 4, Menu.NONE, "RadioGroup");
     	menu.add(0, 5, Menu.NONE, "Spinner");
     	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+		case 0:
+			showAutoComplete();
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		default:
+			break;
+		}
+    	return true;
+    }
+    
+    public void showAutoComplete(){
+    	Intent autocomplete = new Intent(this, AutoComplete.class);
+    	startActivity(autocomplete);
     }
 }
